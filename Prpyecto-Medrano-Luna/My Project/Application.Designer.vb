0b48356de6765815e4fee34c20c063cc26ef3d8b
santiilunaa <santiago.luna3101@gmail.com>
2022-10-01 17:45:58 -0300

    Formulario MDI y Formulario BackUp

--- Prpyecto-Medrano-Luna/My Project/Application.Designer.vb
+++ Prpyecto-Medrano-Luna/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Prpyecto_Medrano_Luna.Form2
+            Me.MainForm = Global.Prpyecto_Medrano_Luna.FormPrinc
         End Sub
     End Class
 End Namespace
