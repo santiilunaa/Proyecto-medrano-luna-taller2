d71f8c631f1fda28835f0451b87d709be4f0a266
santiilunaa <santiago.luna3101@gmail.com>
2022-10-01 11:41:59 -0300

    División de Paneles

--- Prpyecto-Medrano-Luna/Form2.Designer.vb
+++ Prpyecto-Medrano-Luna/Form2.Designer.vb
@@ -26,13 +26,26 @@ Partial Class Form2
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
+        Me.PReportes = New System.Windows.Forms.Panel()
+        Me.PProductos = New System.Windows.Forms.Panel()
+        Me.PClientes = New System.Windows.Forms.Panel()
+        Me.PUsuarios = New System.Windows.Forms.Panel()
+        Me.PBakcUp = New System.Windows.Forms.Panel()
+        Me.PVentas = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
+        Me.PReportes.SuspendLayout()
+        Me.PProductos.SuspendLayout()
+        Me.PClientes.SuspendLayout()
+        Me.PUsuarios.SuspendLayout()
+        Me.PVentas.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
@@ -43,7 +56,7 @@ Partial Class Form2
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1285, 38)
+        Me.Panel1.Size = New System.Drawing.Size(1382, 38)
         Me.Panel1.TabIndex = 0
         '
         'Label1
@@ -74,6 +87,8 @@ Partial Class Form2
         'Panel2
         '
         Me.Panel2.BackColor = System.Drawing.SystemColors.GrayText
+        Me.Panel2.Controls.Add(Me.Button7)
+        Me.Panel2.Controls.Add(Me.Button8)
         Me.Panel2.Controls.Add(Me.Button2)
         Me.Panel2.Controls.Add(Me.Button3)
         Me.Panel2.Controls.Add(Me.Button4)
@@ -84,8 +99,34 @@ Partial Class Form2
         Me.Panel2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Panel2.Location = New System.Drawing.Point(0, 38)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(172, 663)
+        Me.Panel2.Size = New System.Drawing.Size(172, 708)
         Me.Panel2.TabIndex = 1
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button7.Location = New System.Drawing.Point(20, 360)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(152, 50)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button8.Location = New System.Drawing.Point(20, 467)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(152, 50)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Button8"
+        Me.Button8.UseVisualStyleBackColor = False
         '
         'Button2
         '
@@ -93,7 +134,7 @@ Partial Class Form2
         Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button2.Location = New System.Drawing.Point(20, 14)
+        Me.Button2.Location = New System.Drawing.Point(20, 18)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(152, 50)
         Me.Button2.TabIndex = 2
@@ -107,7 +148,7 @@ Partial Class Form2
         Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button3.Location = New System.Drawing.Point(20, 82)
+        Me.Button3.Location = New System.Drawing.Point(20, 84)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(152, 50)
         Me.Button3.TabIndex = 3
@@ -121,7 +162,7 @@ Partial Class Form2
         Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button4.Location = New System.Drawing.Point(20, 140)
+        Me.Button4.Location = New System.Drawing.Point(20, 163)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(152, 50)
         Me.Button4.TabIndex = 4
@@ -135,7 +176,7 @@ Partial Class Form2
         Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button5.Location = New System.Drawing.Point(20, 208)
+        Me.Button5.Location = New System.Drawing.Point(20, 230)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(152, 50)
         Me.Button5.TabIndex = 5
@@ -148,26 +189,90 @@ Partial Class Form2
         Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Button6.Location = New System.Drawing.Point(20, 336)
+        Me.Button6.Location = New System.Drawing.Point(20, 304)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(152, 50)
         Me.Button6.TabIndex = 6
         Me.Button6.Text = "Button6"
         Me.Button6.UseVisualStyleBackColor = False
         '
+        'PReportes
+        '
+        Me.PReportes.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.PReportes.Controls.Add(Me.PProductos)
+        Me.PReportes.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PReportes.Location = New System.Drawing.Point(0, 0)
+        Me.PReportes.Name = "PReportes"
+        Me.PReportes.Size = New System.Drawing.Size(1382, 746)
+        Me.PReportes.TabIndex = 7
+        '
+        'PProductos
+        '
+        Me.PProductos.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.PProductos.Controls.Add(Me.PClientes)
+        Me.PProductos.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PProductos.Location = New System.Drawing.Point(0, 0)
+        Me.PProductos.Name = "PProductos"
+        Me.PProductos.Size = New System.Drawing.Size(1382, 746)
+        Me.PProductos.TabIndex = 0
+        '
+        'PClientes
+        '
+        Me.PClientes.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.PClientes.Controls.Add(Me.PVentas)
+        Me.PClientes.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PClientes.Location = New System.Drawing.Point(0, 0)
+        Me.PClientes.Name = "PClientes"
+        Me.PClientes.Size = New System.Drawing.Size(1382, 746)
+        Me.PClientes.TabIndex = 1
+        '
+        'PUsuarios
+        '
+        Me.PUsuarios.Controls.Add(Me.PBakcUp)
+        Me.PUsuarios.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PUsuarios.Location = New System.Drawing.Point(0, 0)
+        Me.PUsuarios.Name = "PUsuarios"
+        Me.PUsuarios.Size = New System.Drawing.Size(1382, 746)
+        Me.PUsuarios.TabIndex = 1
+        '
+        'PBakcUp
+        '
+        Me.PBakcUp.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PBakcUp.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PBakcUp.Location = New System.Drawing.Point(0, 0)
+        Me.PBakcUp.Name = "PBakcUp"
+        Me.PBakcUp.Size = New System.Drawing.Size(1382, 746)
+        Me.PBakcUp.TabIndex = 1
+        '
+        'PVentas
+        '
+        Me.PVentas.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PVentas.Controls.Add(Me.PUsuarios)
+        Me.PVentas.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PVentas.Location = New System.Drawing.Point(0, 0)
+        Me.PVentas.Name = "PVentas"
+        Me.PVentas.Size = New System.Drawing.Size(1382, 746)
+        Me.PVentas.TabIndex = 1
+        '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1285, 701)
+        Me.ClientSize = New System.Drawing.Size(1382, 746)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PReportes)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Form2"
         Me.Text = "Form2"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         Me.Panel2.ResumeLayout(False)
+        Me.PReportes.ResumeLayout(False)
+        Me.PProductos.ResumeLayout(False)
+        Me.PClientes.ResumeLayout(False)
+        Me.PUsuarios.ResumeLayout(False)
+        Me.PVentas.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -181,4 +286,12 @@ Partial Class Form2
     Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents PReportes As Panel
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents PProductos As Panel
+    Friend WithEvents PClientes As Panel
+    Friend WithEvents PUsuarios As Panel
+    Friend WithEvents PBakcUp As Panel
+    Friend WithEvents PVentas As Panel
 End Class
--- Prpyecto-Medrano-Luna/Form2.vb
+++ Prpyecto-Medrano-Luna/Form2.vb
@@ -2,4 +2,8 @@
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs) Handles PReportes.Paint
+
+    End Sub
 End Class
